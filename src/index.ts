adeae6bfd74a0769e72fa9a170f41c3019890e87
Yasuo Fukuda <yasuo.fukuda@theport.jp>
2020-06-08 10:23:47 +0900

    add editorconfig

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,19 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 //firebase.analytics();
+
+
+/**
+ * test
+// Initialize Firebase with a "default" Firebase project
+var defaultProject = firebase.initializeApp(firebaseConfig);
+
+console.log(defaultProject.name);  // "[DEFAULT]"
+
+// Option 1: Access Firebase services via the defaultProject variable
+var defaultStorage = defaultProject.storage();
+var defaultFirestore = defaultProject.firestore();
+
+// Option 2: Access Firebase services using shorthand notation
+defaultStorage = firebase.storage();
+defaultFirestore = firebase.firestore();
--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,35 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: './src/index.ts',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /・tsx?$/,
-                use: {
-                    loader: "ts-loader",
-                    options: {
-                        transpileOnly: true,
-                    }
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".js", ".ts", ".tsx", ".json"]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "src/index.html")
-        })
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 2000
-    }
+  entry: './src/index.ts',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /・tsx?$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".json"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src/index.html")
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 2000
+  }
 }
